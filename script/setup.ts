8be9a1567cd60523a8842ec0e3446e5424007a4e
Ilyar <761285+ilyar@users.noreply.github.com>
2021-05-29 04:53:26 +0300

    fix: use import consistent

--- script/setup.ts
+++ script/setup.ts
@@ -1,14 +1,11 @@
 import * as near from '../src/'
-
 import * as dotenv from 'dotenv'
 import * as path from 'path'
-import * as account from "../src/account";
-
 dotenv.config({path: path.join(path.dirname(__dirname), 'test.env')})
 
 async function main() {
     const code = await near.fetchContract('near', 'mainnet')
-    const sender = account.parseAccountNetwork('node0')
+    const sender = near.parseAccountNetwork('node0')
     const contract = near.custodianAccount('local')
     await near.createAndDeployContract(contract, code, '1000000', sender)
 }
